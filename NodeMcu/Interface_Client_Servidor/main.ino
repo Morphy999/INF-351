64b15e24190bc7baa9022bcefa20567c1c30749e
Walter bueno <91851813+Morphy999@users.noreply.github.com>
2023-09-30 17:08:58 -0300

    Update main.ino
    adicionando comentarios

--- NodeMcu/Interface_Client_Servidor/main.ino
+++ NodeMcu/Interface_Client_Servidor/main.ino
@@ -2,7 +2,7 @@
 #include <FastLED.h>
 
 #define OUT1 12
-#define LED_PIN     12
+#define LED_PIN     12 //D6
 #define NUM_LEDS    16
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2812
@@ -17,6 +17,7 @@ WiFiServer server(80); //instncia o server na porta 80
 void setup() {
 Serial.begin(115200); //configura comunicação serial
 delay(10);
+//inicializaçao FastLED
 FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
 FastLED.setBrightness(  BRIGHTNESS );
 
@@ -24,6 +25,7 @@ FastLED.setBrightness(  BRIGHTNESS );
 pinMode(OUT1, OUTPUT);
 // mensagem de debug serial
 Serial.print("Conectando para a rede ");
+
 Serial.println(ssid);
 // Iniciando a conexão WiFi
 WiFi.begin(ssid, senha);
@@ -54,6 +56,10 @@ delay(1);
 String req = client.readStringUntil('\r');
 Serial.println(req);
 client.flush();
+
+
+//A partir daqui verifica qual botao foi clickado
+
 if (req.indexOf("ioonred") != -1)
 {
 fill_solid( leds, NUM_LEDS, CRGB::Red);
@@ -74,6 +80,8 @@ FastLED.show();
 statusOUT1 = HIGH;
 }
 
+//Aqui caso tenha clickado em Desligar
+
 else if (req.indexOf("iooff") != -1)
 {
 fill_solid( leds, NUM_LEDS, CRGB::Black);
@@ -81,21 +89,28 @@ FastLED.show();
 statusOUT1 = LOW;
 }
 
+
+//HTML para a Criaçao da pagina
+
 client.println("HTTP/1.1 200 OK");
 client.println("Content-Type: text/html");
 client.println("");
 client.println("<!DOCTYPE HTML>");
 client.println("<html>");
 client.println("<h1>Escolha qual deseja ligar</h1>");
+
+//caso esteja Desligado,aparece o 3 botoes
+
 if(!statusOUT1){
 client.println("<p>RED <a href=\"ioonred\"><button>LIGAR</button></a></p>");
 client.println("<p>BLUE <a href=\"ioonBlue\"><button>LIGAR</button></a></p>");
 client.println("<p>GREEN <a href=\"ioonGreen\"><button>LIGAR</button></a></p>");
 }
+
+//Se tiver ligado,aparece o botao de desligar
 else
 client.println("<p><a href=\"iooff\"><button>DESLIGAR</button></a></p>");
 client.println("</html>");
 delay(10);
 }
-
 
